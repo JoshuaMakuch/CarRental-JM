d0317e423f770101adf021407c266856983388e7
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-03-28 08:35:39 -0600

    Updated the tooltips

--- RentalForm.vb
+++ RentalForm.vb
@@ -1,6 +1,30 @@
-﻿Option Explicit On
+﻿'Joshua Makuch
+'RCET 0265
+'Spring 2023
+'Car Rental Form Fork
+'https://github.com/JoshuaMakuch/CarRental-JM.git
+
+'Things to do:
+'Code for just one customer and accumulate all the necessary data, validate, and then display
+
+Option Explicit On
 Option Strict On
 Option Compare Binary
+
 Public Class RentalForm
+
+    'Asks the user if they want to exit the program and does an appropriate reaction
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+
+        If MessageBox.Show("Do you want to exit?", "Exit Program", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            Me.Close()
+        End If
+
+    End Sub
+
+    'What happens when the user submits their information and also validates that the inputs are correct
+    Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+
+    End Sub
 
 End Class
